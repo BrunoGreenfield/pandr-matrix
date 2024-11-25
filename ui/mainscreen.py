4eb8c63770af0e43f5620f9e3cd40cc16de2f85e
Bruno Greenfield <brunogreenfielduk@gmail.com>
2024-11-25 17:01:47 +0000

    PAN-13: Added blank tabs to ui

--- ui/mainscreen.py
+++ ui/mainscreen.py
@@ -1,10 +1,20 @@
 import tkinter as tk
+from tkinter import ttk
 
 window = tk.Tk()
 window.title('Matrix Control Panel')
 window.minsize(width=1024, height=768) # We will want to change this to the resolution of our monitor
 
-exitBtn = tk.Button(window, text='Exit', command=window.quit, width=20)
+notebook = ttk.Notebook(window) # Manages a collection of windows/displays
+
+mainTab = tk.Frame(notebook)
+baseNTab = tk.Frame(notebook)
+
+notebook.add(mainTab, text='Home')
+notebook.add(baseNTab, text='BaseN')
+notebook.pack(expand=True, fill='both') # Expand fills space not otherwise used, fill will fill space on x and y axis
+
+exitBtn = tk.Button(mainTab, text='Exit', command=window.quit, width=20)
 exitBtn.pack()
 
 window.mainloop()
