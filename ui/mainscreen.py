688871b60d9ef881dbeebfc1a1cf90c687ffee89
ThreepennyLlama <90915821+ThreepennyLlama@users.noreply.github.com>
2024-11-26 16:28:44 +0000

    Update mainscreen.py

--- ui/mainscreen.py
+++ ui/mainscreen.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+from utils.bin_dec_hex_gen import randBin
+
+def displaybin():
+    bindisplay.config(text=randBin())
 
 window = tk.Tk()
 window.title('Matrix Control Panel')
@@ -16,5 +20,11 @@ notebook.pack(expand=True, fill='both') # Expand fills space not otherwise used,
 
 exitBtn = tk.Button(mainTab, text='Exit', command=window.quit, width=20)
 exitBtn.pack()
+
+binaryBtn = tk.Button(baseNTab, text='Binary', command=displaybin, width=20)
+binaryBtn.pack()
+
+bindisplay = tk.Label(baseNTab)
+bindisplay.pack()
 
 window.mainloop()
