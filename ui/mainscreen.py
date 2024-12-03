84b2eeeb8a6ed583218974625dbc47e7538491ac
Bruno Greenfield <brunogreenfielduk@gmail.com>
2024-12-03 09:32:21 +0000

    Added full-screen attribute for when running on the final product

--- ui/mainscreen.py
+++ ui/mainscreen.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 window = tk.Tk()
 window.title('Matrix Control Panel')
 window.minsize(width=1280, height=800)
+# window.attributes("-fullscreen", True) # Uncomment this for when running on the Pi
 
 notebook = ttk.Notebook(window) # Manages a collection of windows/displays
 
