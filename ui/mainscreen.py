de8b18b0c9ced68669b083fef1c6a143b6ecb5f2
ThreepennyLlama <90915821+ThreepennyLlama@users.noreply.github.com>
2024-12-10 16:34:51 +0000

    Update mainscreen.py

--- ui/mainscreen.py
+++ ui/mainscreen.py
@@ -8,12 +8,17 @@ window.minsize(width=1280, height=800)
 
 notebook = ttk.Notebook(window) # Manages a collection of windows/displays
 
+style = ttk.Style()
+style.theme_use('default')
+
+style.configure("TNotebook.Tab", padding=[10, 20])
+
 mainTab = tk.Frame(notebook)
 baseNTab = tk.Frame(notebook)
 
 notebook.add(mainTab, text='Home')
 notebook.add(baseNTab, text='BaseN')
-notebook.pack(expand=True, fill='both') # Expand fills space not otherwise used, fill will fill space on x and y axis
+notebook.pack(expand=True, fill='both')
 
 exitBtn = tk.Button(mainTab,
                     text='Exit',
@@ -23,6 +28,6 @@ exitBtn = tk.Button(mainTab,
                     activeforeground="red",
                     command=window.quit,
                     width=20)
-exitBtn.place(x=492, y=710)
+exitBtn.place(x=560, y=710)
 
 window.mainloop()
