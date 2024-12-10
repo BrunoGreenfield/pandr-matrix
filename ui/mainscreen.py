7bd3be84ecbf39eb59557a9cb1bcc614d3dc02ba
Bruno Greenfield <brunogreenfielduk@gmail.com>
2024-12-10 14:24:40 +0000

    PAN-11: Undo merge

--- ui/mainscreen.py
+++ ui/mainscreen.py
@@ -21,15 +21,8 @@ notebook.add(mainTab, text='Home')
 notebook.add(baseNTab, text='BaseN')
 notebook.pack(expand=True, fill='both') # Expand fills space not otherwise used, fill will fill space on x and y axis
 
-exitBtn = tk.Button(mainTab,
-                    text='Exit',
-                    bg="red",
-                    fg="white",
-                    activebackground="white",
-                    activeforeground="red",
-                    command=window.quit,
-                    width=20)
-exitBtn.place(x=492, y=710)
+exitBtn = tk.Button(mainTab, text='Exit', command=window.quit, width=20)
+exitBtn.pack()
 
 #Binary Gen
 binaryBtn = tk.Button(baseNTab, text='Binary', command=displaybin, width=30, height=5).pack(side="top")
