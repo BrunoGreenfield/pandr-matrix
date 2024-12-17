67e019ad52ea18703492726ea9bc1a53374342cf
Rivulet <zdaw2011@gmail.com>
2024-12-17 16:25:47 +0000

    PAN-19 Added announcement feature

--- ui/mainscreen.py
+++ ui/mainscreen.py
@@ -3,8 +3,7 @@ from tkinter import ttk
 
 window = tk.Tk()
 window.title('Matrix Control Panel')
-window.minsize(width=1280, height=800)
-# window.attributes("-fullscreen", True) # Uncomment this for when running on the Pi
+window.minsize(width=1024, height=768) # We will want to change this to the resolution of our monitor
 
 notebook = ttk.Notebook(window) # Manages a collection of windows/displays
 
@@ -14,6 +13,24 @@ baseNTab = tk.Frame(notebook)
 notebook.add(mainTab, text='Home')
 notebook.add(baseNTab, text='BaseN')
 notebook.pack(expand=True, fill='both') # Expand fills space not otherwise used, fill will fill space on x and y axis
+
+announcementVar=tk.StringVar()
+def submit():
+    announcement=announcementVar.get()
+    #("    ANNOUNCEMENT    " + announcement + "    ANNOUNCEMENT    ")
+    screenPrev.config(text=announcement)
+    announcementVar.set("")
+    
+announcementEntry = tk.Entry(mainTab,
+                             textvariable = announcementVar,
+                             font=('DejaVu Sans',10,'normal'))
+subBtn=tk.Button(mainTab,text = 'Submit', command = submit)
+screenPrev = tk.Label(mainTab)
+
+announcementEntry.pack()
+subBtn.pack()
+screenPrev.pack()
+
 
 exitBtn = tk.Button(mainTab,
                     text='Exit',
